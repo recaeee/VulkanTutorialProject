103f764d0f2ca2010094718357cf9e25d1e2a9ff
recaeee <recaeee@163.com>
2023-10-05 00:22:47 +0800

    Drawing a triangle_ set up

---
+++ Chapter_DrawingATriangle/DrawingATriangle/DrawingATriangle/Main.cpp
@@ -0,0 +1,60 @@
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+#include <stdexcept>
+#include <cstdlib>
+
+class HelloTriangleApplication
+{
+public:
+	void run()
+	{
+		initWindow();
+		initVulkan();
+		mainLoop();
+		cleanup();
+	}
+
+private:
+	GLFWwindow* window;
+
+	void initWindow()
+	{
+		glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+		glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+		window = glfwCreateWindow(800, 600, "Vulkan", nullptr, nullptr);
+	}
+
+	void initVulkan()
+	{
+
+	}
+
+	void mainLoop()
+	{
+
+	}
+
+	void cleanup()
+	{
+
+	}
+};
+
+int main()
+{
+	HelloTriangleApplication app;
+
+	try 
+	{
+		app.run();
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	return EXIT_SUCCESS;
+}
